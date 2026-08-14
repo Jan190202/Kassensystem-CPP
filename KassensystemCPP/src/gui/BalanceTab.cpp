4294d418e2e1fce47115db8ddbfbe17d14e32c22
Jan Schoeppe <schoeppejan.misc@gmail.com>
2026-08-14 23:05:34 +0200

    added nicer layout to BalanceTab

--- KassensystemCPP/src/gui/BalanceTab.cpp
+++ KassensystemCPP/src/gui/BalanceTab.cpp
@@ -1,97 +1,143 @@
 #include "BalanceTab.h"
 
-#include <QTableView>
-#include <QPushButton>
-#include <QLabel>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QGridLayout>
 #include <QDate>
-#include <QFrame>
+#include <QFormLayout>
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QSizePolicy>
+#include <QTableView>
+#include <QVBoxLayout>
 
 BalanceTab::BalanceTab(QWidget* parent) : BaseTab(parent) {}
 
 void BalanceTab::initialize()
 {
-	// temporary
+	// Temporary
 	dateBefore = QDate(2000, 1, 1);
 
-	// buttons
-	auto* btnAddSpending	= new QPushButton(tr("+"), this);
-	auto* btnAddEarning		= new QPushButton(tr("+"), this);
+	// Buttons
+	auto* btnAddEarning		= new QPushButton(tr("+ Einnahme"), this);
+	auto* btnAddSpending	= new QPushButton(tr("+ Ausgabe"), this);
 
-	// tables
+	// Tables
 	tblEarnings		= new QTableView(this);
 	tblSpendings	= new QTableView(this);
 
-	// labels
-	lCashBefore			= new QLabel(tr("0.00 €"));
-	lCashDifference		= new QLabel(tr("0.00 €"));
-	lCashAfter			= new QLabel(tr("0.00 €"));
+	tblEarnings->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+	tblSpendings->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-	lSavingsBefore		= new QLabel(tr("0.00 €"));
-	lSavingsDifference	= new QLabel(tr("0.00 €"));
-	lSavingsAfter		= new QLabel(tr("0.00 €"));
+	// Labels
+	lCashBefore			= new QLabel(tr("0,00 €"), this);
+	lCashDifference		= new QLabel(tr("0,00 €"), this);
+	lCashAfter			= new QLabel(tr("0,00 €"), this);
 
-	lEarnings			= new QLabel(tr("0.00 €"));
-	lSpendings			= new QLabel(tr("0.00 €"));
+	lSavingsBefore		= new QLabel(tr("0,00 €"), this);
+	lSavingsDifference	= new QLabel(tr("0,00 €"), this);
+	lSavingsAfter		= new QLabel(tr("0,00 €"), this);
 
-	// layouting
-	auto* mainLayout				= new QVBoxLayout(this);
-	auto* tableLayout				= new QHBoxLayout();
-	auto* tableEarningsLayout		= new QVBoxLayout();
-	auto* tableEarningsLowerLayout	= new QHBoxLayout();
-	auto* tableSpendingsLayout		= new QVBoxLayout();
-	auto* tableSpendingsLowerLayout	= new QHBoxLayout();
-	auto* summaryLayout				= new QHBoxLayout();
-	auto* summaryBeforeLayout		= new QGridLayout();
-	auto* summaryDifferenceLayout	= new QGridLayout();
-	auto* summaryAfterLayout		= new QGridLayout();
+	lEarnings			= new QLabel(tr("0,00 €"), this);
+	lSpendings			= new QLabel(tr("0,00 €"), this);
 
+	const auto configureAmount = [](QLabel* label)
+		{
+			label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+			label->setMinimumWidth(90);
+		};
 
-	mainLayout->addLayout(tableLayout);
-	tableLayout->addLayout(tableEarningsLayout);
-	auto* vLine = new QFrame(); vLine->setFrameShape(QFrame::VLine);
-	tableLayout->addWidget(vLine);
-	tableLayout->addLayout(tableSpendingsLayout);
-	auto* hLine = new QFrame(); hLine->setFrameShape(QFrame::HLine);
-	mainLayout->addWidget(hLine);
+	configureAmount(lCashBefore);
+	configureAmount(lCashDifference);
+	configureAmount(lCashAfter);
+	configureAmount(lSavingsBefore);
+	configureAmount(lSavingsDifference);
+	configureAmount(lSavingsAfter);
+	configureAmount(lEarnings);
+	configureAmount(lSpendings);
+
+	// earnings
+	auto* earningsBox = new QGroupBox(tr("Einnahmen"), this);
+	auto* earningsLayout = new QVBoxLayout(earningsBox);
+	earningsLayout->setContentsMargins(12, 16, 12, 12);
+	earningsLayout->setSpacing(10);
+
+	auto* earningsFooterLayout = new QHBoxLayout();
+	earningsFooterLayout->setContentsMargins(0, 0, 0, 0);
+	earningsFooterLayout->addWidget(new QLabel(tr("Gesamt:"), earningsBox));
+	earningsFooterLayout->addWidget(lEarnings);
+	earningsFooterLayout->addStretch();
+	earningsFooterLayout->addWidget(btnAddEarning);
+
+	earningsLayout->addWidget(tblEarnings, 1);
+	earningsLayout->addLayout(earningsFooterLayout);
+
+	// spendings
+	auto* spendingsBox = new QGroupBox(tr("Ausgaben"), this);
+	auto* spendingsLayout = new QVBoxLayout(spendingsBox);
+	spendingsLayout->setContentsMargins(12, 16, 12, 12);
+	spendingsLayout->setSpacing(10);
+
+	auto* spendingsFooterLayout = new QHBoxLayout();
+	spendingsFooterLayout->setContentsMargins(0, 0, 0, 0);
+	spendingsFooterLayout->addWidget(new QLabel(tr("Gesamt:"), spendingsBox));
+	spendingsFooterLayout->addWidget(lSpendings);
+	spendingsFooterLayout->addStretch();
+	spendingsFooterLayout->addWidget(btnAddSpending);
+
+	spendingsLayout->addWidget(tblSpendings, 1);
+	spendingsLayout->addLayout(spendingsFooterLayout);
+
+	// before
+	auto* beforeBox = new QGroupBox(
+		tr("Stand %1").arg(dateBefore.toString(QStringLiteral("dd.MM.yyyy"))),
+		this);
+
+	auto* beforeLayout = new QFormLayout(beforeBox);
+	beforeLayout->setContentsMargins(12, 16, 12, 12);
+	beforeLayout->setHorizontalSpacing(16);
+	beforeLayout->setVerticalSpacing(7);
+	beforeLayout->addRow(tr("Bestand:"), lSavingsBefore);
+	beforeLayout->addRow(tr("Bar:"), lCashBefore);
+
+	// difference
+	auto* differenceBox = new QGroupBox(tr("Differenz"), this);
+
+	auto* differenceLayout = new QFormLayout(differenceBox);
+	differenceLayout->setContentsMargins(12, 16, 12, 12);
+	differenceLayout->setHorizontalSpacing(16);
+	differenceLayout->setVerticalSpacing(7);
+	differenceLayout->addRow(tr("Bestand:"), lSavingsDifference);
+	differenceLayout->addRow(tr("Bar:"), lCashDifference);
+
+	// after
+	auto* afterBox = new QGroupBox(
+		tr("Stand %1").arg(dateAfter.toString(QStringLiteral("dd.MM.yyyy"))),
+		this);
+
+	auto* afterLayout = new QFormLayout(afterBox);
+	afterLayout->setContentsMargins(12, 16, 12, 12);
+	afterLayout->setHorizontalSpacing(16);
+	afterLayout->setVerticalSpacing(7);
+	afterLayout->addRow(tr("Bestand:"), lSavingsAfter);
+	afterLayout->addRow(tr("Bar:"), lCashAfter);
+
+	// main layout
+	auto* tableLayout = new QHBoxLayout();
+	tableLayout->setContentsMargins(0, 0, 0, 0);
+	tableLayout->setSpacing(14);
+	tableLayout->addWidget(earningsBox, 1);
+	tableLayout->addWidget(spendingsBox, 1);
+
+	auto* summaryLayout = new QHBoxLayout();
+	summaryLayout->setContentsMargins(0, 0, 0, 0);
+	summaryLayout->setSpacing(14);
+	summaryLayout->addWidget(beforeBox, 1);
+	summaryLayout->addWidget(differenceBox, 1);
+	summaryLayout->addWidget(afterBox, 1);
+
+	auto* mainLayout = new QVBoxLayout(this);
+	mainLayout->setContentsMargins(18, 18, 18, 18);
+	mainLayout->setSpacing(14);
+	mainLayout->addLayout(tableLayout, 1);
 	mainLayout->addLayout(summaryLayout);
-	summaryLayout->addLayout(summaryBeforeLayout);
-	summaryLayout->addLayout(summaryDifferenceLayout);
-	summaryLayout->addLayout(summaryAfterLayout);
-	
-	tableEarningsLayout->addWidget(new QLabel(tr("Einnahmen")));
-	tableEarningsLayout->addWidget(tblEarnings);
-	tableEarningsLayout->addLayout(tableEarningsLowerLayout);
-	tableEarningsLowerLayout->addWidget(new QLabel(tr("Gesamt:")));
-	tableEarningsLowerLayout->addWidget(lEarnings);
-	tableEarningsLowerLayout->addWidget(btnAddEarning);
-
-	tableSpendingsLayout->addWidget(new QLabel(tr("Ausgaben")));
-	tableSpendingsLayout->addWidget(tblSpendings);
-	tableSpendingsLayout->addLayout(tableSpendingsLowerLayout);
-	tableSpendingsLowerLayout->addWidget(new QLabel(tr("Gesamt:")));
-	tableSpendingsLowerLayout->addWidget(lSpendings);
-	tableSpendingsLowerLayout->addWidget(btnAddSpending);
-
-	summaryBeforeLayout->addWidget(new QLabel(tr("Stand ") + dateBefore.toString("dd.MM.yyyy")), 0, 0, 1, 2);
-	summaryBeforeLayout->addWidget(new QLabel(tr("Bestand:")),						1, 0, 1, 1);
-	summaryBeforeLayout->addWidget(lSavingsBefore,									1, 1, 1, 1);
-	summaryBeforeLayout->addWidget(new QLabel(tr("Bar:")),							2, 0, 1, 1);
-	summaryBeforeLayout->addWidget(lCashBefore,										2, 1, 1, 1);
-	
-	summaryDifferenceLayout->addWidget(new QLabel(tr("Differenz")),					0, 0, 1, 2);
-	summaryDifferenceLayout->addWidget(new QLabel(tr("Bestand:")),					1, 0, 1, 1);
-	summaryDifferenceLayout->addWidget(lSavingsDifference,							1, 1, 1, 1);
-	summaryDifferenceLayout->addWidget(new QLabel(tr("Bar:")),						2, 0, 1, 1);
-	summaryDifferenceLayout->addWidget(lCashDifference,								2, 1, 1, 1);
-
-
-	summaryAfterLayout->addWidget(new QLabel(tr("Stand ") + dateAfter.toString("dd.MM.yyyy")),	0, 0, 1, 2);
-	summaryAfterLayout->addWidget(new QLabel(tr("Bestand:")),						1, 0, 1, 1);
-	summaryAfterLayout->addWidget(lSavingsAfter,									1, 1, 1, 1);
-	summaryAfterLayout->addWidget(new QLabel(tr("Bar:")),							2, 0, 1, 1);
-	summaryAfterLayout->addWidget(lCashAfter,										2, 1, 1, 1);
-
 }
